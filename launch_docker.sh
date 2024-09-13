5d7b29f5784f1b9fe5247af9418ac17945def70e
Heng <31088159+shiukaheng@users.noreply.github.com>
2024-09-13 01:51:01 +0100

    user permissions fixed

--- launch_docker.sh
+++ launch_docker.sh
@@ -30,20 +30,27 @@ done
 # Get the hostname or IP
 HOSTNAME=$(get_hostname)
 
+# Get current user's UID and GID
+HOST_UID=$(id -u)
+HOST_GID=$(id -g)
+
 # Print information
 echo -e "${BLUE}Launching Docker container on:${NC}"
 echo -e "${GREEN}SSH port: ${SSH_PORT}${NC}"
 echo -e "${GREEN}Web port: ${WEB_PORT}${NC}"
 echo -e "${GREEN}Access URL: http://${HOSTNAME}:${WEB_PORT}${NC}"
+echo -e "${GREEN}Using UID: ${HOST_UID}, GID: ${HOST_GID}${NC}"
 
 # Create a named volume for XDG_RUNTIME_DIR
 docker volume create xdg_runtime_dir
 
 # Launch the Docker container
 docker run -it --rm \
+  --user ${HOST_UID}:${HOST_GID} \
   -p ${SSH_PORT}:22 \
   -p ${WEB_PORT}:8000 \
   -v $(realpath ./):/home/developer/repo \
-  -v xdg_runtime_dir:/run/user/1000 \
+  -v xdg_runtime_dir:/run/user/${HOST_UID} \
   -e DISPLAY=:0 \
+  -e HOME=/home/developer \
   rbc_dev_ros:latest
