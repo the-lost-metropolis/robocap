6578f5076d0094ca6fcabd0b2c061b5786ace9bd
ugsfume <86786290+ugsfume@users.noreply.github.com>
2024-09-15 21:25:35 +0100

    robot frame tested in Rviz

--- src/robocap_sim/setup.py
+++ src/robocap_sim/setup.py
@@ -10,6 +10,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include the urdf directory and its contents
+        ('share/' + package_name + '/urdf', ['urdf/frame_ultra_low_poly.stl']),
+        # If you have other files in the urdf directory, include them like this
+        # ('share/' + package_name + '/urdf', ['urdf/robot.urdf.xacro', 'urdf/inertial_macros.xacro', ...]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
