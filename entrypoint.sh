35df1fe38901c657ed1de0b9af2be2cf9d6c431c
Heng <31088159+shiukaheng@users.noreply.github.com>
2024-09-15 23:38:47 +0100

    add entrypoint to reduce mental overhead to figure out how to resume progress

---
+++ entrypoint.sh
@@ -0,0 +1,3 @@
+# This file defines the entrypoint of Robocap
+# Right now, this is just running the simulation template provided by https://github.com/hijimasa/omni_wheel_controller_sample
+ros2 launch sample_robot_sim sample_robot_spawn.launch.py
